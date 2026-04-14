e21dc96ae386172d1e23f888217779d820d37956
sosuisen <hidekazu.kubota@gmail.com>
2026-04-15 00:31:32 +0900

    docs: fix comment

--- Writer.java
+++ Writer.java
@@ -10,23 +10,23 @@ public class Writer {
         /*
          * MemoMapオブジェクトはWriterクラスの外部で生成され、
          * コンストラクタ経由で渡されて、フィールドに保持されます。
-         * この構造は「集約」（aggregation）と呼ばれます。
+         * この関係は「集約」（aggregation）と呼ばれます。
          * 
          * このとき、MemoMapオブジェクトは、必ず外部からも参照されています。
-         * 結果として、WriterオブジェクトがGCの対象になっても、
-         * MemoMapオブジェクトはGCの対象になりません。
+         * 結果として、WriterオブジェクトがGC候補になっても、
+         * MemoMapオブジェクトはGC候補になりません。
          */
         this.memoMap = memoMap;
 
         /*
          * MemoListオブジェクトはWriterクラスの内部で生成され、
          * フィールドに保持されます。
-         * この構造は「コンポジション」（composition）と呼ばれます。
+         * この関係は「コンポジション」（composition）と呼ばれます。
          * 
-         * このとき、MemoListオブジェクトは、privateにして、getterも持たない限り、
+         * MemoListオブジェクトは、privateかつgetterも持たない限り、
          * 外部からは参照されません。
-         * この場合、WriterオブジェクトがGCの対象になると、
-         * MemoListオブジェクトもGCの対象になります。
+         * この場合、WriterオブジェクトがGC候補になると、
+         * MemoListオブジェクトもGC候補になります。
          */
         this.memoList = new MemoList();
         cleaner.register(memoList, () -> IO.println("memoList(composition) is destructed."));
@@ -35,18 +35,19 @@ public class Writer {
     /*
      * dateは、このメソッド内でしか利用されないローカル変数で、
      * 代入した文字列オブジェクトも、このメソッド内でしか利用されません。
-     * よって、メソッドの処理が終われば、すぐにGCの対象になります。
+     * よって、メソッドの処理が終われば、すぐにGC候補になります。
      * (寿命が短い)
      *
      * 一方、フィールドのオブジェクト（memoMap, memoList）は、
-     * WriterオブジェクトがGCの対象になるまで、GCの対象になりません。
+     * 少なくともWriterオブジェクトがGC候補になるまでは、GC候補になりません。
      * （寿命が長い）
      * このおかげで、memoMapとmemoListは複数のメソッドで利用することができます。
      * （addLine, printの両方で利用）
      *
      * フィールドのオブジェクトは便利ですが、いろんな場所から呼び出すことが
      * できるため、どこかで予想外の変更が加えられる可能性があります。
-     * つまり、クラス設計では、フィールドのオブジェクトの寿命により注目する必要があります。
+     * 一般に、クラス設計では、ローカル変数よりもフィールドのオブジェクトの寿命について
+     * より注目する必要があります。
      */
     public void addLine(String line) {
         var date = java.time.LocalDate.now().toString();
